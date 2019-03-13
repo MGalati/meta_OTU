1a55c3af3f51e37ab81740e0c8732aee4c4873b0
fconstancias <florentinconstancias@gmail.com>
2019-03-13 13:23:43 +0100

    Create 0_fastqc.sh

---
+++ 0_fastqc.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#$ -q normal.q
+#$ -N fastqc
+#$ -M florentin.constancias@cirad.fr
+#$ -pe parallel_smp 6
+#$ -l mem_free=6G
+#$ -V
+#$ -cwd
+#$ -V
+
+
+module purge
+module load system/conda/5.1.0
+source activate fastqc_multiqc
+
+# JOB BEGIN
+
+for IN in BP CP DD IB II LU MC
+do
+mkdir $IN'qc/'
+fastqc -t $NSLOTS $IN*fastq* -o $IN'qc/'
+multiqc $IN'qc/' -o $IN'qc/'
+done
+
+
+# JOB END
+date
+
+exit 0
